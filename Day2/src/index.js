96c951fb025dc23794868af0e21b8c82ae852a4c
ZaidDarweesh <zaid@quiqflow.com>
2025-09-30 13:27:49 +0300

    Day2: Modular JavaScript & GitHub PR Workflow

---
+++ Day2/src/index.js
@@ -0,0 +1,4 @@
+import { add, subtract } from './math.js';
+const a = 10, b = 4;
+console.log(`add(${a}, ${b}) =`, add(a, b));
+console.log(`subtract(${a}, ${b}) =`, subtract(a, b));
---
+++ Day2/src/math.js
@@ -0,0 +1,2 @@
+export function add(a, b) { return Number(a) + Number(b); }
+export function subtract(a, b) { return Number(a) - Number(b); }
